3e0582ae415c9183a9eb900bc8b6acc2f9089872
Hidetaka <hidetaka.48691001@icloud.com>
2015-09-16 18:41:08 +0900

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
 
-	def show
+  def index
+		@users = User.all
+  end
+
+  #def index
+  #@users = User.paginate(page: params[:page])
+  #end
+  
+   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+   def show
     @user = User.find(params[:id])
   end
 
@@ -22,6 +39,21 @@ class UsersController < ApplicationController
     end
   end
 
+   def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 
 
   private
@@ -29,5 +61,24 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
+
+   def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+
+   # Before actions
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
 
