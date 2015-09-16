999b966f99c9074b48bfa5d5f38da8df8879f692
Hidetaka <hidetaka.48691001@icloud.com>
2015-09-16 17:08:15 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     def create
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
+    	sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -26,8 +27,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
 end
 
