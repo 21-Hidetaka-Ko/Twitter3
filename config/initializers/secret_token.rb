c7e9ee50ea4f884c16450cce9ecbabd2518c0f7e
Hidetaka <hidetaka.48691001@icloud.com>
2015-09-17 23:00:29 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitter3::Application.config.secret_key_base = '3693a5968f000ea4bedafbcd227999c268b2f2b75f0e90a2caf01efd11044b7faf4fafcff72cd3f9aedfec04191beaac759a12052deb631cfb6cd7b9957ed772'
